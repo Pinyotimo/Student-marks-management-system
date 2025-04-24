479e655a9908ff555a464ec99437d4a5cdf07ef8
Stephen Oduor <steveotieno2012005@gmail.com>
2025-04-24 16:53:53 +0300

    Update main.c

--- src/main.c
+++ src/main.c
@@ -23,26 +23,56 @@ void showMenu() {
 
 int main() {
     int choice;
+
     // Load students on program start
-    loadStudentsFromFile();
+    if (!loadStudentsFromFile()) { // Assume loadStudentsFromFile returns a boolean for success
+        printf("Warning: Failed to load student data. Starting with an empty database.\n");
+    }
 
     do {
         showMenu();
-        scanf("%d", &choice);
-        getchar(); // clear input buffer
+        
+        // Input validation for menu choice
+        if (scanf("%d", &choice) != 1) {
+            printf("Invalid input. Please enter a number.\n");
+            while (getchar() != '\n'); // Clear the input buffer
+            continue;
+        }
 
         switch (choice) {
-            case 1: addStudent(); break;
-            case 2: displayStudents(); break;
-            case 3: searchStudent(); break;
-            case 4: updateMarks(); break;
-            case 5: deleteStudent(); break;
-            case 6: exportToCSV(); break;
-            case 7: generateReportCards(); break;
-            case 8: modifyGradingSystem(); break;
-            case 9: viewPerformanceAnalytics(); break;
-            case 10: printf("Exiting...\n"); break;
-            default: printf("Invalid choice. Try again.\n");
+            case 1: 
+                addStudent(); 
+                break;
+            case 2: 
+                displayStudents(); 
+                break;
+            case 3: 
+                searchStudent(); 
+                break;
+            case 4: 
+                updateMarks(); 
+                break;
+            case 5: 
+                deleteStudent(); 
+                break;
+            case 6: 
+                exportToCSV(); 
+                break;
+            case 7: 
+                generateReportCards(); 
+                break;
+            case 8: 
+                modifyGradingSystem(); 
+                break;
+            case 9: 
+                viewPerformanceAnalytics(); 
+                break;
+            case 10: 
+                printf("Saving data and exiting...\n");
+                saveStudentsToFile(); // Save data before exiting
+                break;
+            default: 
+                printf("Invalid choice. Try again.\n");
         }
     } while (choice != 10);
 
